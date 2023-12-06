17f2dedf1fe3111c0fdb5280ee148621f1921ab4
Dav999 <dav999.tolp@gmail.com>
2023-12-06 04:36:50 +0100

    Update for Ved 1.11.0
    Ved now reads and saves the <TextboxColours> and <SpecialRoomnames>
    itself. This means Future should no longer do the same thing -
    otherwise tags would get duplicated in level files. vvvvvvxmltemplate
    is now renamed on purpose to prevent this from happening without
    warning.
    
    This also adds a little protection against including the Future plugin
    itself multiple times - the "-- updated 1.11.0" comment is removed,
    meaning it can only be found and replaced a single time.

--- info.lua
+++ info.lua
@@ -4,7 +4,7 @@ t.shortname = "Future"  -- The name that will be displayed on the button in the 
 t.longname = "Future"  -- This can be about twice as long
 t.author = "AllyTally"  -- Your name
 t.version = "1.0-alpha"  -- The current version of this plugin, can be anything you want
-t.minimumved = "1.10.0"  -- The minimum version of Ved this plugin is designed to work with. If unsure, just use the latest version.
+t.minimumved = "1.11.0"  -- The minimum version of Ved this plugin is designed to work with. If unsure, just use the latest version.
 t.description = [[
 Support for VVVVVV PRs which may or may not get merged in the future.
 ]]  -- The description that will be displayed in the plugins list. This uses the help/notepad system, so you can use text formatting here, and even images!
